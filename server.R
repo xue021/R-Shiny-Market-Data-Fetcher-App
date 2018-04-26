739edc81523643de97278f7625e43ea038c6a149
xue021 <37210622+xue021@users.noreply.github.com>
2018-04-26 00:10:39 -0400

    Add files via upload
    updated

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
 
 function(input, output) {
   
+  
+  
+  
   data = reactive({
     library(quantmod)
     symbol = selectionName()
@@ -12,53 +15,92 @@ function(input, output) {
     t = as.data.frame(getSymbols(symbol,src=source,auto.assign=FALSE,from=dateFrom,to=dateTo))
     t$Date = rownames(t)
     t = t[,c(7,1:5)]
-    colnames(t)=c("Date","Open","High","Low","Close","Volume")
+    colnames(t)=c("date","open","high","low","close","volume")
     rownames(t)=1:nrow(t)
+    t[,2:ncol(t)] = round(t[,2:ncol(t)],2)
     return(t)
   })
+  
   indicatorData = reactive({
     data = data()
     
+    data$date = as.Date(data$date)
+    data$close = as.numeric(data$close)
+    data$open = as.numeric(data$open)
+    data$high = as.numeric(data$high)
+    data$low = as.numeric(data$low)
+    data$volume = as.numeric(data$volume)
+    hlc = data.frame(data$high,data$low,data$close)
     
-    data$Date = as.Date(data$Date)
-    data$Close = as.numeric(data$Close)
-    data$High = as.numeric(data$High)
-    data$Low = as.numeric(data$Low)
-    data$Volume = as.numeric(data$Volume)
-    hlc = data.frame(data$High,data$Low,data$Close)
-    
+    indicatorsSelected = indicators()
+    print("inds")
     library("TTR")
-    data$sma10 = SMA(x = data$Close,n=10)
-    data$sma20 = SMA(x = data$Close,n=20)
     
-    macd = MACD(x=data$Close, nFast = 5, nSlow = 26, nSig = 20, percent = TRUE)
-    data$macd = macd[,1]
+    if('0' %in% indicatorsSelected){
+      macd = MACD(x=data$close, nFast = 5, nSlow = 26, nSig = 20, percent = TRUE)
+      data$macd = macd[,1]
+    }
+    stoch_14_3_3 = stoch(HLC=data$close, nFastK = 14, nFastD = 3, nSlowD = 3, bounded = TRUE,smooth = 1)
+    if('1' %in% indicatorsSelected){
+      data$stochfast = stoch_14_3_3[,1]*100
+    }
     
-    
-    
-    stoch_14_3_3 = stoch(HLC=data$Close, nFastK = 14, nFastD = 3, nSlowD = 3, bounded = TRUE,smooth = 1)
-    data$stochfast = stoch_14_3_3[,1]*100
-    data$stochslow = stoch_14_3_3[,3]*100
-    
-    smi_13_2_25 = SMI(HLC=hlc, n = 13, nFast = 2, nSlow = 25, nSig = 9, bounded = TRUE,smooth = 1)
-    data$smi = smi_13_2_25[,1]
-    
-    stoch_volume = stoch(HLC=data$Volume, nFastK = 14, nFastD = 3, nSlowD = 3, bounded = TRUE,smooth = 1)
-    data$stoch_volume = stoch_volume[,1]*100
-    
-    rsi = RSI(data$Close)
-    data$rsi = rsi
-    
+    if('2' %in% indicatorsSelected){
+      data$stochslow = stoch_14_3_3[,3]*100
+    }
+    if('3' %in% indicatorsSelected){
+      stoch_volume = stoch(HLC=data$volume, nFastK = 14, nFastD = 3, nSlowD = 3, bounded = TRUE,smooth = 1)
+      data$stoch_volume = stoch_volume[,1]*100
+    }
+    if('4' %in% indicatorsSelected){
+      smi_13_2_25 = SMI(HLC=hlc, n = 13, nFast = 2, nSlow = 25, nSig = 9, bounded = TRUE,smooth = 1)
+      data$smi = smi_13_2_25[,1]
+    }
+    if('5' %in% indicatorsSelected){
+      rsi = RSI(data$close)
+      data$rsi = rsi
+    }
     adx = ADX(hlc)
-    data$adx_DX = adx[,3]
-    data$adx_ADX = adx[,4]
+    if('6' %in% indicatorsSelected){
+      
+      data$dmi_dx = adx[,3]
+    }
+    if('7' %in% indicatorsSelected){
+      data$dmi_adx = adx[,4]
+    }
+    if('y' %in% indicatorsSelected){
+      data = data[which(complete.cases(data)),]
+    }
     
-    output = data.frame(data$Date,data$Close,data[,9:16])
-    output = output[which(complete.cases(output)),]
-    colnames(output)=c("Date","Close","macd","stochfast","stochslow","smi","stoch_volume","rsi","adx_DX","adx_ADX")
+    output = data.frame(data[,1:2],data[,7:ncol(data)])
+    
+    #colnames(output)=c("date","close","macd","stochfast","stochslow","smi","stoch_volume","rsi","dmi_dx","dmi_adx")
     output[,3:ncol(output)]= round(output[,3:ncol(output)],2)
     return(output)
   })
+  
+  indicatorMetaData = reactive({
+    inddata = indicatorData()
+    metadata = data.frame(colnames(inddata))
+    colnames(metadata)="column"
+    metadata$max = 0
+    metadata$min = 0
+    
+    for(i in 2:nrow(metadata)){
+      metadata$max[i] = max(inddata[,i])
+      
+    }
+    for(i in 2:nrow(metadata)){
+      metadata$min[i] = min(inddata[,i])
+      
+    }
+    for(i in 1:nrow(metadata)){
+      metadata$datatype[i] = class(inddata[,i])
+      
+    }
+    return(metadata)
+  })
+  
   selectionName = eventReactive(input$selectData,{
     return(input$symInput)
   })
@@ -69,13 +111,15 @@ function(input, output) {
   to = eventReactive(input$selectData,{
     return(input$dateTo)
   })
-  
+  indicators = eventReactive(input$selectData,{
+    return(input$indicatorsSelected)
+  })
   output$stockDataTable = DT::renderDataTable({
     return(data())
   })
   output$stockIndicatorDataTable = DT::renderDataTable({
     out = indicatorData()
-    colnames(out)=c("Date","Close","macd","stochfast","stochslow","smi","stoch_vol","rsi","dmi","avg. dmi")
+    #colnames(out)=c("Date","Close","macd","stochfast","stochslow","smi","stoch_vol","rsi","dmi","avg. dmi")
     return(out)
   })
   
@@ -95,13 +139,23 @@ function(input, output) {
       write.csv(indicatorData(), file, row.names = FALSE)
     }
   )
+  output$downloadMetadata = downloadHandler(
+    filename = function() {
+      paste(selectionName(),"_marketdata_metadata", ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(indicatorMetaData(), file, row.names = FALSE)
+    }
+  )
  
 
-  output$daterange=renderText({
+  output$debug=renderText({
     from=from()
     to = to()
     asset = selectionName()
-    out = paste("Asset:",asset,"\nFrom:",from,"\nTo:",to)
+    indsStr = paste(indicators(),collapse = ", ")
+    print(class(indsStr))
+    out = paste("Asset:",asset,"\nFrom:",from,"\nTo:",to,"\nIndicators Selected: ",indsStr)
     return(out)
   })
  
--- ui.R
+++ ui.R
@@ -9,23 +9,30 @@ fluidPage(
       offset = 1,
       textInput("symInput", label = h3("Enter stock symbol:"), value = "TWTR"),
       tags$br(),
-      
+      dateInput("dateFrom","From:",value="2008-04-24"),
+      dateInput("dateTo","To:",value="2018-04-24"),
       tags$br(),
       actionButton("selectData", "Fetch Data"),
-      tags$br()#,
-      #actionButton("testButton","Test Button")
-      
+      tags$br()
       
       ),
       column(
         4,
         offset = 2,
-        dateInput("dateFrom","From:",value="2008-04-24"),
-        dateInput("dateTo","To:",value="2018-04-24"),
-        #tags$b("What data would you like to see?"),
-        tags$br(),
-        tags$br()
-       
+        
+        checkboxGroupInput("indicatorsSelected", "Include Indicators:",
+                           c("Moving Average Convergence Divergence (MACD)" = 0,
+                             "Stochastic (fast)" = 1,
+                             "Stochastic (slow)" = 2,
+                             "Stochastic Volume" = 3,
+                             "Stochastic Movement Index (SMI)" = 4,
+                             "Relative Strength Index (RSI)" = 5,
+                             "Directional Movement Index (Signal)" = 6,
+                             "Directional Movement Index (Average Signal)" = 7,
+                             "Remove Incomplete Rows (Suggested)"= "y"),
+                           selected = c(0,1,2,3,4,5,6,7,"y")
+        )
+        
         
         
       )
@@ -34,16 +41,22 @@ fluidPage(
         column(
           10,
           offset = 1,
-          tags$h4("Currently Selected:"),
+          tags$h4("Currently Selected Symbol:"),
           tags$h1(textOutput("showSelection")),
-          verbatimTextOutput("daterange"),
-          downloadButton("downloadRaw", "Download Raw Data"),
-          downloadButton("downloadIndicator", "Download Indicator Data"),
+          #verbatimTextOutput("debug"),
+          
           tags$br(),
           tags$br(),
           tabsetPanel(
             tabPanel("Raw Stock Data",tags$b("Yahoo Finance Stock Data"),DT::dataTableOutput("stockDataTable")),
-            tabPanel("Indicator Data",tags$b("Indicator Data:"),tags$b("Indicator Data:"),DT::dataTableOutput("stockIndicatorDataTable")))
+            tabPanel("Indicator Data",tags$b("Indicator Data:"),tags$b("Indicator Data:"),DT::dataTableOutput("stockIndicatorDataTable"))),
+          downloadButton("downloadRaw", "Download Raw Data"),
+          downloadButton("downloadIndicator", "Download Indicator Data"),
+          downloadButton("downloadMetadata", "Download Min/Max Indicator Values"),
+          tags$br(),
+          tags$i("min/max for use with cs487 project"),
+          tags$i("above project can be found at https://github.com/xue021/cs487groupproject")
+          
         )
        
     # sidebarPanel(
